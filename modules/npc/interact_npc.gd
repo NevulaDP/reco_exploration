e3d4e25e50a6aaddde7ee4aaa2f10e0605e92ab8
NevulaDP <nevobetesh@gmail.com>
2024-06-24 22:49:47 +0300

    Player initail state machine initialized and working

--- modules/npc/interact_npc.gd
+++ modules/npc/interact_npc.gd
@@ -3,6 +3,7 @@ class_name InteractNPC
 
 @onready var interaction_area = $InteractionArea
 @export var timeline: DialogicTimeline
+signal interaction_end
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,9 +15,13 @@ func _process(delta):
 	pass
 
 func _on_interact():
+	var state_chart = get_tree().get_first_node_in_group("player").get_node("%StateChart")
+	state_chart.send_event("is_talking")
 	Dialogic.timeline_ended.connect(_on_timeline_ended)
 	await Dialogic.start(timeline)
 	get_viewport().set_input_as_handled()
 
 func _on_timeline_ended():
 	InteractionManager.can_interact=true
+	var state_chart = get_tree().get_first_node_in_group("player").get_node("%StateChart")
+	state_chart.send_event("ended_talking")
--- scripts/exploration/player_movement.gd
+++ scripts/exploration/player_movement.gd
@@ -5,17 +5,21 @@ class_name PlayerMovement
 const tile_size:int = 64
 var moving:bool = false
 var input_dir : Vector2
+var speed:int = 1
 #Character
 @export var actor: CharacterBody2D #player node
 @onready var animation_tree = $"../AnimationTree"
 @onready var anim_state = animation_tree.get("parameters/playback")
+@onready var state_chart = %StateChart
+
 
 
 func _physics_process(delta):
+	print(input_dir)
 	input_dir=Vector2(Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left"),
 	Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up")).normalized()
 	
-	actor.velocity = input_dir*delta*3000
+	actor.velocity = input_dir*delta*3000*speed
 	if input_dir !=Vector2.ZERO:
 		anim_state.travel("walk")
 		animation_tree.set("parameters/walk/blend_position",input_dir)
@@ -25,3 +29,14 @@ func _physics_process(delta):
 		anim_state.travel("idle")
 	actor.move_and_slide()
 
+
+
+func _on_talking_state_entered():
+	input_dir=Vector2.ZERO
+	actor.velocity=Vector2.ZERO
+	anim_state.travel("idle")
+	self.set_physics_process(false)
+
+
+func _on_walking_state_entered():
+	self.set_physics_process(true)
