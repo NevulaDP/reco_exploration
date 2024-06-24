389de0e6e42ecf7c9b68d9813dd94a70d48d78b6
NevulaDP <nevobetesh@gmail.com>
2024-06-24 16:48:01 +0300

    Character Controller & Tilesetting

---
+++ scripts/exploration/player_movement.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+class_name PlayerMovement
+
+const tile_size:int = 64
+var moving:bool = false
+var input_dir : Vector2
+#Character
+@export var actor: CharacterBody2D #player node
+@onready var animation_tree = $"../AnimationTree"
+@onready var anim_state = animation_tree.get("parameters/playback")
+
+
+func _physics_process(delta):
+	input_dir=Vector2(Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left"),
+	Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up")).normalized()
+	
+	actor.velocity = input_dir*delta*3000
+	if input_dir !=Vector2.ZERO:
+		anim_state.travel("walk")
+		animation_tree.set("parameters/walk/blend_position",input_dir)
+		animation_tree.set("parameters/idle/blend_position",input_dir)
+	else:
+		actor.velocity=Vector2.ZERO
+		anim_state.travel("idle")
+	actor.move_and_slide()
